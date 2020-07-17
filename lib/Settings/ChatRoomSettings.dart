479d97bf46531fb0e2c6e2d7b7bb90a652a6d458
joelngyx <joelngyx@gmail.com>
2020-07-17 20:09:48 +0800

    updated as of 17 july 8 09pm

--- lib/Settings/ChatRoomSettings.dart
+++ lib/Settings/ChatRoomSettings.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:orbital_2020_usono_my_ver/Services/database/RoomDbService.dart';
 import 'package:orbital_2020_usono_my_ver/Shared/constants.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatRoomSettingsForm extends StatefulWidget {
   @override
@@ -11,6 +13,21 @@ class _ChatRoomSettingsFormState extends State<ChatRoomSettingsForm> {
   String _currentRoomName = '';
   final _formKey = GlobalKey<FormState>();
   final RoomDbService _database = new RoomDbService();
+
+  Position _location = Position(latitude: 0.0, longitude: 0.0);
+  void _getCurrentLocation(String roomid) async {
+    final location = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      _location = location;
+    });
+
+    Firestore.instance.collection('Rooms').document(roomid).setData({
+      'Latitude': _location.latitude,
+      'Longitude': _location.longitude,
+    }, merge: true);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +56,7 @@ class _ChatRoomSettingsFormState extends State<ChatRoomSettingsForm> {
                 // create a collection which is to be named after _currentRoomName
                 String roomID =
                     await _database.createChatRoom(_currentRoomName);
-
+                _getCurrentLocation(roomID);
                 Navigator.pushNamed(context, '/ChatRoomPage', arguments: {
                   "roomName": _currentRoomName,
                   "roomID": roomID
