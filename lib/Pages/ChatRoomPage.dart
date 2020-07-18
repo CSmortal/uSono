a289f76b25d207e23d23407de3cc3900ac1b6873
CSmortal <jovenpua51@gmail.com>
2020-07-18 21:18:31 +0800

    Fixed bottom overflow for AnonSignIn and SignIn, can now retrieve messages in rooms too

--- lib/Pages/ChatRoomPage.dart
+++ lib/Pages/ChatRoomPage.dart
@@ -60,6 +60,7 @@ class _ChatRoomPageState extends State<ChatRoomPage> with TickerProviderStateMix
               flex: 8,
               child: StreamBuilder<QuerySnapshot>(
                 stream: dbService.getRoomMessages(),
+//                stream: Firestore.instance.collection('Rooms').document(roomID).collection('Messages').orderBy("time").snapshots(),
                 builder: (context, snapshot) {
 
                   if (!snapshot.hasData) {
@@ -70,15 +71,15 @@ class _ChatRoomPageState extends State<ChatRoomPage> with TickerProviderStateMix
                     );
                   } else {
                     return ListView.builder(
-                      reverse: true,
                       itemCount: snapshot.data.documents.length,
                       itemBuilder: (context, index) {
-                        return Message(
-                            text: snapshot.data.documents[index].data["text"],
-                            animationController: new AnimationController( // still need to do the animation
-                                vsync: this,
-                                duration: new Duration(milliseconds: 800))
-                        );
+//                        return Message(
+//                            text: snapshot.data.documents[index].data["text"],
+//                            animationController: new AnimationController( // still need to do the animation
+//                                vsync: this,
+//                                duration: new Duration(milliseconds: 800))
+//                        );
+                        return Text("${snapshot.data.documents[index].data["text"]}");
                       },
 
                     );
--- lib/Pages/SignIn.dart
+++ lib/Pages/SignIn.dart
@@ -43,6 +43,8 @@ class _SignInState extends State<SignIn> {
                   ),
                 ),
                 child: Scaffold(
+                  resizeToAvoidBottomInset: false, // Lazy way of avoiding bottom overflow when keyboard appears
+                  // 2nd solution: https://medium.com/zipper-studios/the-keyboard-causes-the-bottom-overflowed-error-5da150a1c660
 //                  backgroundColor: Colors.brown[100],
                   appBar: AppBar(
                     // leading: Container(width: 36, color: Colors.red),
@@ -64,82 +66,88 @@ class _SignInState extends State<SignIn> {
                     centerTitle: true,
                   ),
 
-                  body: Container(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-                    child: Form(
-                      key: _formKey,
-                      child: Column(
-                        children: [
-                          SizedBox(height: 20),
-                          TextFormField(
-                              decoration: textInputDecoration.copyWith(
-                                  hintText: 'Email'),
-                              validator: (val) =>
-                                  val.isEmpty ? 'Enter an email' : null,
-                              onChanged: (val) {
-                                setState(() => email = val);
-                              }),
-                          SizedBox(height: 20),
-                          TextFormField(
-                              decoration: textInputDecoration.copyWith(
-                                  hintText: 'Password'),
-                              obscureText: true,
-                              validator: (val) => val.length < 6
-                                  ? 'Enter a password 6+ chars long'
-                                  : null,
-                              onChanged: (val) {
-                                setState(() => password = val);
-                              }),
-                          SizedBox(height: 20),
-                          Column(children: [
-                            RaisedButton(
-                                color: Colors.pink[400],
-                                child: Text('Sign In',
-                                    style: TextStyle(color: Colors.white)),
-                                onPressed: () async {
-                                  if (_formKey.currentState.validate()) {
-                                    // validates form based on form's state, using the validator properties in each FormField
-                                    setState(() {
-                                      // show loading widget if form is valid
-                                      loading = true;
-                                    });
+                  body: Column(
+                    children: [
+                      // Padding(padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0)),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                          child: Form(
+                              key: _formKey,
+                              child: Column(
+                                children: [
+                                  SizedBox(height: 20),
+                                  TextFormField(
+                                      decoration: textInputDecoration.copyWith(
+                                          hintText: 'Email'),
+                                      validator: (val) =>
+                                      val.isEmpty ? 'Enter an email' : null,
+                                      onChanged: (val) {
+                                        setState(() => email = val);
+                                      }),
+                                  SizedBox(height: 20),
+                                  TextFormField(
+                                      decoration: textInputDecoration.copyWith(
+                                          hintText: 'Password'),
+                                      obscureText: true,
+                                      validator: (val) => val.length < 6
+                                          ? 'Enter a password 6+ chars long'
+                                          : null,
+                                      onChanged: (val) {
+                                        setState(() => password = val);
+                                      }),
+                                  SizedBox(height: 20),
+                                  Column(children: [
+                                    RaisedButton(
+                                        color: Colors.pink[400],
+                                        child: Text('Sign In',
+                                            style: TextStyle(color: Colors.white)),
+                                        onPressed: () async {
+                                          if (_formKey.currentState.validate()) {
+                                            // validates form based on form's state, using the validator properties in each FormField
+                                            setState(() {
+                                              // show loading widget if form is valid
+                                              loading = true;
+                                            });
 
-                                    // entire Form is valid if all validators return null
-                                    dynamic result =
-                                        await _auth.signInWithEmailAndPassword(
-                                            email, password);
+                                            // entire Form is valid if all validators return null
+                                            dynamic result =
+                                            await _auth.signInWithEmailAndPassword(
+                                                email, password);
 
-                                    if (result == null) {
-                                      print("No record of this user");
-                                      setState(() {
-                                        error =
-                                            'COULD NOT SIGN IN WITH THOSE CREDENTIALS';
-                                        loading =
-                                            false; // no longer display loading widget if ...
-                                      });
-                                    }
-                                  }
-                                }),
-                            SizedBox(height: 12),
-                            RaisedButton(
-                                color: Colors.pink[400],
-                                child: Text('Guest Sign In',
-                                    style: TextStyle(color: Colors.white)),
-                                onPressed: () {
-                                  setState(() {
-                                    anon = true;
-                                  });
-                                }),
-                          ]),
-                          SizedBox(height: 12),
-                          Text(
-                            error,
-                            style: TextStyle(color: Colors.red, fontSize: 14),
-                          ),
-                        ],
-                      ),
-                    ),
+                                            if (result == null) {
+                                              print("No record of this user");
+                                              setState(() {
+                                                error =
+                                                'COULD NOT SIGN IN WITH THOSE CREDENTIALS';
+                                                loading =
+                                                false; // no longer display loading widget if ...
+                                              });
+                                            }
+                                          }
+                                        }),
+                                    SizedBox(height: 12),
+                                    RaisedButton(
+                                        color: Colors.pink[400],
+                                        child: Text('Guest Sign In',
+                                            style: TextStyle(color: Colors.white)),
+                                        onPressed: () {
+                                          setState(() {
+                                            anon = true;
+                                          });
+                                        }),
+                                  ]),
+                                  SizedBox(height: 12),
+                                  Text(
+                                    error,
+                                    style: TextStyle(color: Colors.red, fontSize: 14),
+                                  ),
+                                ],
+                              ),
+                            ),
+                        ),
+                        ),
+                    ],
                   ),
                 ),
               );
